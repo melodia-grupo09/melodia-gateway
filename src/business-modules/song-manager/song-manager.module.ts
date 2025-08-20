3fd8f6bb31d712e253f9c6aaf6892c02a4205ef1
Martín Bocanegra <ebocanegra@fi.uba.ar>
2025-08-20 02:12:03 -0300

    fix song-manager dependencies

--- src/business-modules/song-manager/song-manager.module.ts
+++ src/business-modules/song-manager/song-manager.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
 import { SongModule } from "src/entity-modules/song/song.module";
+import { SongsController } from "./song-manager.controller";
+import { GetSongsUseCase } from "./use-cases/get-songs.use-case";
+import { CreateSongUseCase } from "./use-cases/create-song.use-case";
 
 @Module({
-  controllers: [],
+  controllers: [SongsController],
   providers: [
+    GetSongsUseCase,
+    CreateSongUseCase,
     
   ],
   imports: [SongModule],
