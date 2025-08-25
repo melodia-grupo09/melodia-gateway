97f7a45ad927e375caf3fadf38886caf8d42f891
Martín Bocanegra <ebocanegra@fi.uba.ar>
2025-08-24 21:09:45 -0300

    setup: add exception filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import mikroOrmConfig from 'mikro-orm.config';
 import { SongManagerModule } from './business-modules/song-manager/song-manager.module';
-import { ResponseFormatInterceptor } from './interceptors/response-format.interceptor';
+import { ResponseFormatInterceptor } from './framework/interceptors/response-format.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
