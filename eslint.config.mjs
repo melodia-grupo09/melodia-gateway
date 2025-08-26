4cfa545decbf1a000634e9614abadacdffc2b994
Martín Bocanegra <ebocanegra@fi.uba.ar>
2025-08-25 22:53:57 -0300

    ci: add lint on commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -20,15 +20,23 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: import.meta.url
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "unused-imports/no-unused-imports": "warn",
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+  }
 );
