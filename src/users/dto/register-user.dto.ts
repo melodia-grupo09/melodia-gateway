59ad8e6fdcb45c65ec2921e753adc2f5c65486d9
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-23 15:31:17 -0300

    Add password pattern matching

--- src/users/dto/register-user.dto.ts
+++ src/users/dto/register-user.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterUserDto {
   @ApiProperty({
@@ -28,6 +34,16 @@ export class RegisterUserDto {
   @IsNotEmpty({ message: 'Password is required' })
   @IsString({ message: 'Password must be a string' })
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  /**
+   * Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character
+   */
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{6,}$/,
+    {
+      message:
+        'Password must be at least 6 characters long and include uppercase, lowercase, number, and special character',
+    },
+  )
   password: string;
 }
 
