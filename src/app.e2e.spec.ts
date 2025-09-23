ccdfe5adc0ace34328c075c98aff85b9104f09c2
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-23 14:07:17 -0300

    Add new app tests

---
+++ src/app.e2e.spec.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from './app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe('/ (GET)', () => {
+    it('should return 200 with status ok wrapped in data object', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect({ data: { status: 'ok' } });
+    });
+
+    it('should have correct response headers', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+  });
+});
