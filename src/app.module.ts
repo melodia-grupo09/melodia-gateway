87f600a7226aebcf0566995cde4c440d5b84ae6f
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-09-24 13:36:09 -0300

    Add ConfigModule to manage environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+  ],
   providers: [],
 })
 export class AppModule {}
