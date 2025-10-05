0d2d3f931d23a2de5eec7ab8f360c68c99b8e7ee
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-10-05 18:24:55 -0300

    Configure artists module to use ARTISTS_SERVICE_URL environment variable

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,10 +1,19 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ArtistsController } from './artists.controller';
 import { ArtistsService } from './artists.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        baseURL: configService.get<string>('ARTISTS_SERVICE_URL') ?? '',
+        timeout: 10000,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
