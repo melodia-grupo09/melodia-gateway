e47b179166b27f8a3165dcf672ffe6a59d1dcabe
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-11-10 10:48:55 -0300

    Add artist external service url to release module

--- src/releases/releases.module.ts
+++ src/releases/releases.module.ts
@@ -1,10 +1,19 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ReleasesController } from './releases.controller';
 import { ReleasesService } from './releases.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        baseURL: configService.get<string>('ARTISTS_SERVICE_URL') ?? '',
+        timeout: 10000,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ReleasesController],
   providers: [ReleasesService],
   exports: [ReleasesService],
